13bda39660df13c676f346321411371d287911d0
Abigail Douglas <EburgAbby@aol.com>
2022-12-09 05:07:44 -0800

    ids, varchars in schema

--- db/schema.sql
+++ db/schema.sql
@@ -9,8 +9,12 @@ USE tracker_db;
 CREATE TABLE department (
 
     -- id: INT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT,
 
     -- name: VARCHAR(30) to hold department name
+    name VARCHAR(30) NOT NULL,
+
+    PRIMARY KEY (id)
 
 );
 
@@ -20,12 +24,18 @@ CREATE TABLE department (
 CREATE TABLE role (
 
     -- id: INT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT,
 
     -- title: VARCHAR(30) to hold role title
+    title VARCHAR(30) NOT NULL,
 
     -- salary: DECIMAL to hold role salary
 
     -- department_id: INT to hold reference to department role belongs to
+
+
+
+    PRIMARY KEY (id)
 
 );
 
@@ -35,16 +45,24 @@ CREATE TABLE role (
 CREATE TABLE employee (
 
     -- id: INT PRIMARY KEY
+    id INT NOT NULL AUTO_INCREMENT,
 
     -- first_name: VARCHAR(30) to hold employee first name
+    first_name VARCHAR(30) NOT NULL,
 
     -- last_name: VARCHAR(30) to hold employee last name
+    last_name VARCHAR(30) NOT NULL,
 
     -- role_id: INT to hold reference to employee role
 
     -- manager_id: INT to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 
-);
 
+
+
+
+    PRIMARY KEY (id)
+
+);
 
 
