0f9ea2e76a8f998c38a69525b62df83ce02f6e9e
Abigail Douglas <EburgAbby@aol.com>
2022-12-14 03:53:49 -0800

    trying torestrucure

--- index.js
+++ index.js
@@ -1,171 +1,27 @@
 // PACKAGES REQUIRED -------------------------------------------------------------
 // FOR IN TEMINAL QUESTIONS
 const inquirer = require("inquirer");
-// FOR CREATING FILES
-const fs = require("fs");
-// FOR EMPLOYEE CONSTRUCTOR
-const determineAction = require("./lib/builder");
-// // deptArray ARRAY
-// const deptArray = require("../db/dept");
-// // roleArray ARRAY
-// const roleArray = require("../db/role");
-// // empArray ARRAY
-// const empArray  = require("../db/emp");
+const initQs = require("./lib/questions");
 
-
-
-// QUESTION ARRAYS ---------------------------------------------------------------
-// STARTER QUESTIONS ON LOAD
-// WHEN I start the application THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-const initQuestions = [
-  {
-    type: "list",
-    name: "action",
-    message: "Welcome to the Employee Tracker Application!\n  You can exit this menu at any time with Ctrl+C \n  What would you like to do?",
-    choices: [
-        "View all Departments",
-        "View all Roles",
-        "View all Employees",
-        "Add a Department",
-        "Add a Role",
-        "Add an Employee",
-        "Update an Employee Role",
-    ],
-    
-  },
-
-  // WHEN I choose to add a department
-  // THEN I am prompted to enter the name of the department and that department is added to the database
-  {
-    type: "input",
-    name: "addDepartment.name",
-    message: "Okay! Enter the name of the new department: ",
-
-    when(answers) {
-        return answers.action === "Add a Department"
-    }
-  },
-
-  // WHEN I choose to add a role
-  // THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-  {
-    type: "input",
-    name: "addRole.name",
-    message: "Okay! Enter the name of the new role: ",
-
-    when(answers) {
-        return answers.action === "Add a Role"
-    }
-  },
-  {
-    type: "input",
-    name: "addRole.salary",
-    message: "Enter the annual salary for the new role: ",
-
-    when(answers) {
-        return answers.action === "Add a Role"
-    }
-  },
-  {
-    type: "input",
-    name: "addRole.department",
-    message: "What department should this role be under?\n  Don't see the department you need? Restart the app and add a new department.",
-
-    when(answers) {
-        return answers.action === "Add a Role"
-    }
-  },
-
-  // WHEN I choose to add an employee
-  // THEN I am prompted to enter the employee’s first name, last name, role, and manager, and that employee is added to the database
-  {
-    type: "input",
-    name: "addEmployee.firstName",
-    message: "Okay! Enter the employee's first name: ",
-
-    when(answers) {
-        return answers.action === "Add an Employee"
-    }
-  },
-  {
-    type: "input",
-    name: "addEmployee.lastName",
-    message: "Enter the employee's last name: ",
-
-    when(answers) {
-        return answers.action === "Add an Employee"
-    }
-  },
-  {
-    type: "list",
-    name: "addEmployee.role",
-    message: "What role should this new employee have?\n  Don't see the role you need? Restart the app and add a new role.",
-    choices: [
-      "test role",
-    ],
-
-    when(answers) {
-        return answers.action === "Add an Employee"
-    }
-  },
-  {
-    type: "list",
-    name: "addEmployee.manager",
-    message: "Who is this new employee's manager?\n  Don't see the employee you need? Restart the app and add a new employee.",
-    choices: [
-      "test employee",
-    ],
-
-    when(answers) {
-        return answers.action === "Add an Employee"
-    }
-  },
-
-
-  // WHEN I choose to update an employee role
-  // THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
-  {
-    type: "list",
-    name: "updateEmployeeRole.employee",
-    message: "Okay! Which employee needs their role to be updated? ",
-    choices: [
-      "test employee",
-    ],
-
-    when(answers) {
-        return answers.action === "Update an Employee Role"
-    }
-  },
-  {
-    type: "list",
-    name: "updateEmployeeRole.newRole",
-    message: "What should their role be updated to? ",
-    choices: [
-      "test role",
-    ],
-
-    when(answers) {
-        return answers.action === "Update an Employee Role"
-    }
-  },
-]
 
 
 // RUN ON LOAD -------------------------------------------------------------------
 init();
+
+
 
 // QUESTION FUNCTIONS ------------------------------------------------------------
 // INITIALIZE APP & TRIGGER Q's 
 function init() {
 
     // ASK QUESTION ARRAY
-    inquirer.prompt(initQuestions)
+    inquirer.prompt(initQs)
     .then((answers) => {
 
-        console.log(answers);
+      console.log(answers);
 
-        // DETERMINE HOW TO USE DATA
-        determineAction(answers);
+      // DETERMINE HOW TO USE DATA
+      determineAction(answers);
 
     })
     .catch((error) => {
@@ -175,5 +31,54 @@ function init() {
             console.log(error)
         }
     });
+
+}
+
+
+
+// FUNCTION TO DETERMINE AND EXECUTE NEEDED ACTIONS ----------------------------
+function determineAction (answers) {
+
+  // DISPLAY ALL DEPARTMENTS
+  if (answers.action === "View all Departments") {
+
+      console.table();
+
+  // DISPLAY ALL ROLES
+  } else if (answers.action === "View all Roles") {
+
+      console.table();
+
+  // DISPLAY ALL EMPLOYEES
+  } else if (answers.action === "View all Employees") {
+
+      console.table();
+
+  // ADD A DEPARTMENT
+  } else if (answers.action === "Add a Department") {
+
+      new Department
+
+
+
+
+  // ADD A ROLE
+  } else if (answers.action === "Add a Role") {
+
+
+  // ADD AN EMPLOYEE
+  } else if (answers.action === "Add an Employee") {
+
+
+      
+  // UPDATE EMPLOYEE ROLE
+  } else if (answers.action === "Update an Employee Role") {
+      
+
+  // ERROR
+  } else {
+
+      console.log("ERROR! answers.action does not equal anything from the list. It is: " + answers.action);
+  }
 
 }
